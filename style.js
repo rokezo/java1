60fd050758fe36d859fdfdd7900c9770114d8976
rokezo <154547810+rokezo@users.noreply.github.com>
2024-05-15 21:57:41 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,32 @@
+1
+// var name = prompt("введіть своє ім'я:");
+
+// alert("Привіт, " + name);
+2
+// const currentYear = new Date().getFullYear();
+// var birthYear = prompt("Введіть свій рік народження:");
+// var age = currentYear - birthYear;
+// alert("Вам " + age + " років.");
+3
+// var sideLength = prompt("Введіть довжину сторони квадрата:");
+// var perimeter = 4 * sideLength;
+// alert("Периметр квадрата: " + perimeter);
+4
+// var radius = prompt("Введіть радіус кола:");
+// var area = Math.PI * radius * radius;
+// alert("Площа кола: " + area);
+5
+// var distance = prompt("Введіть відстань між містами у кілометрах:");
+// var time = prompt("Введіть час подорожі у годинах:");
+// var speed = distance / time;
+// alert("Вам потрібно рухатися зі швидкістю " + speed + " км/год.");
+6
+// const exchangeRate = 0.85;
+// var dollars = prompt("Введіть суму в доларах:");
+// var euros = dollars * exchangeRate;
+// alert(dollars + " доларів дорівнює " + euros + " євро.");
+7
+// var flashDriveSize = prompt("Введіть об'єм флешки у ГБ:");
+// var fileSize = 820; // Розмір файлу в МБ
+// var filesCount = Math.floor(flashDriveSize * 1024 / fileSize);
+// alert("На флешці об'ємом " + flashDriveSize + " ГБ поміститься " + filesCount + " файлів розміром 820 МБ.");
